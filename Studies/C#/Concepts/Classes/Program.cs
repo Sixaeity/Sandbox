d6d2a7bcaa437509e788a7417b1c32266a243824
Sixaeity <72029537+Sixaeity@users.noreply.github.com>
2021-02-15 20:31:56 +0700

    Classes

---
+++ Studies/C#/Concepts/Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Concepts.Classes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+        /*
+        public void Declaration()
+        {
+            // Declaration is creation instance of class by using 'new' operator or assignation another reference type value.
+            Example example = new Example();
+            Example example1 = example;
+        }
+        */
+
+    }
+
+    /*
+    // Access modifier, class, name.
+    public class Example
+    {
+        // There is body of the class.
+    }
+    */
+
+    /*
+    // This class is inherited from 'Example' class. 
+    public class Inheritance : Example
+    {
+    }
+    */
+}
